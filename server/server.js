efdc71cc04c477b6d43d576f4373830fa42c12aa
Shaheer Mir <shaheermir0@gmail.com>
2017-05-13 13:14:20 -0400

    Finished Step 2 - REST lions.

--- server/server.js
+++ server/server.js
@@ -1,27 +1,68 @@
 // TODO: make this work.
 // if yuo go to localhost:3000 the app
 // there is expected crud to be working here
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var _ = require('lodash');
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var _ = require('lodash')
 
 // express.static will serve everything
 // with in client as a static resource
 // also, it will server the index.html on the
 // root of that directory on a GET to '/'
-app.use(express.static('client'));
+app.use(express.static('client'))
 
 // body parser makes it possible to post JSON to the server
 // we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-
-var lions = [];
-var id = 0;
+var lions = []
+var id = 0
 
 // TODO: make the REST routes to perform CRUD on lions
 
-app.listen(3000);
-console.log('on port 3000');
+app.get('/lions', function (req, res) {
+  res.json(lions)
+})
+
+app.get('/lions/:id', function (req, res) {
+  const lion = lions.find(lion => lion.id === req.params.id)
+  res.json(lion || {})
+})
+
+app.post('/lions', function (req, res) {
+  const lion = req.body
+  id++
+  lion.id = id + ''
+  lions.push(lion)
+  res.status(201).json(lion)
+})
+
+app.put('/lions/:id', function (req, res) {
+  const update = req.body
+  if (update.id) delete update.id
+
+  const index = lions.findIndex(lion => lion.id === req.params.id)
+  if (!lions[index]) {
+    res.send()
+  } else {
+    const updatedLion = Object.assign(lions[index], update)
+    res.json(updatedLion)
+  }
+})
+
+app.delete('/lions/:id', function (req, res) {
+  const index = lions.findIndex(lion => lion.id === req.params.id)
+
+  if (!lions[index]) {
+    res.send()
+  } else {
+    const deletedLion = lions[index]
+    lions.splice(index, 1)
+    res.json(deletedLion)
+  }
+})
+
+app.listen(3000)
+console.log('on port 3000')
