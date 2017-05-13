99d040e783661cd3afdb11ebdeda18187da30064
Shaheer Mir <shaheermir0@gmail.com>
2017-05-13 15:25:39 -0400

    Finished Step 4 - Tigers API

--- server/lions.js
+++ server/lions.js
@@ -1,58 +1,57 @@
-var lionRouter = require('express').Router();
+var lionRouter = require('express').Router()
 
-var lions = [];
-var id = 0;
+var lions = []
+var id = 0
 
-var updateId = function(req, res, next) {
+var updateId = function (req, res, next) {
   if (!req.body.id) {
-    id++;
-    req.body.id = id + '';
+    id++
+    req.body.id = id + ''
   }
-  next();
-};
+  next()
+}
 
-lionRouter.param('id', function(req, res, next, id) {
-  var todo = _.find(todos, {id: id});
+lionRouter.param('id', function (req, res, next, id) {
+  var lion = _.find(lions, { id: id })
 
-  if (todo) {
-    req.todo = todo;
-    next();
+  if (lion) {
+    req.lion = lion
+    next()
   } else {
-    res.send();
+    res.send()
   }
-});
+})
 
-lionRouter.get('/', function(req, res){
-  res.json(lions);
-});
+lionRouter.get('/', function (req, res) {
+  res.json(lions)
+})
 
-lionRouter.get('/:id', function(req, res){
-  var lion = req.todo;
-  res.json(lion || {});
-});
+lionRouter.get('/:id', function (req, res) {
+  var lion = req.lion
+  res.json(lion || {})
+})
 
-lionRouter.post('/', updateId, function(req, res) {
-  var lion = req.body;
+lionRouter.post('/', updateId, function (req, res) {
+  var lion = req.body
 
-  lions.push(lion);
+  lions.push(lion)
 
-  res.json(lion);
-});
+  res.json(lion)
+})
 
-
-lionRouter.put('/:id', function(req, res) {
-  var update = req.body;
+lionRouter.put('/:id', function (req, res) {
+  var update = req.body
   if (update.id) {
     delete update.id
   }
 
-  var lion = _.findIndex(lions, {id: req.params.id});
+  var lion = _.findIndex(lions, { id: req.params.id })
   if (!lions[lion]) {
-    res.send();
+    res.send()
   } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
+    var updatedLion = _.assign(lions[lion], update)
+    res.json(updatedLion)
   }
-});
+})
 
-module.exports = lionRouter;
+module.exports = lionRouter
--- server/tigers.js
+++ server/tigers.js
@@ -2,3 +2,55 @@
 // and make some REST routes for it, exactly like for lions
 // make a middleware that just logs the word 'tiger' to the console
 // when a request comes in to the server
+
+const tigerRouter = require('express').Router()
+
+let id = 0
+const tigers = []
+
+tigerRouter.param('id', function (req, res, next, id) {
+  const tiger = tigers.find(tiger => tiger.id === id)
+  if (tiger) {
+    req.tiger = tiger
+    next()
+  } else {
+    next(new Error('No Tiger Found For The Given ID'))
+  }
+})
+
+tigerRouter.get('/', function (req, res) {
+  res.json(tigers)
+})
+
+tigerRouter.get('/:id', function (req, res) {
+  res.json(req.tiger || {})
+})
+
+function updateId (req, res, next) {
+  id++
+  req.body.id = id + ''
+  next()
+}
+
+tigerRouter.post('/', updateId, function (req, res) {
+  const tiger = req.body
+  tigers.push(tiger)
+  res.json(tiger)
+})
+
+tigerRouter.put('/:id', function (req, res, next) {
+  const update = req.body
+  const index = tigers.findIndex(tiger => tiger.id === req.params.id)
+
+  const updatedTiger = Object.assign(tigers[index], update)
+  res.json(updatedTiger)
+})
+
+tigerRouter.delete('/:id', function (req, res, next) {
+  const index = tigers.findIndex(tiger => tiger.id === req.params.id)
+  const deletedTiger = tigers[index]
+  tigers.splice(index, 1)
+  res.json(deletedTiger)
+})
+
+module.exports = tigerRouter
