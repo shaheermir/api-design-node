3a26b7621071b39c24d7024c372c4e293ef77059
Shaheer Mir <shaheermir0@gmail.com>
2017-05-18 18:54:15 -0400

    Finished Step 11 - Basic Auth

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -1,9 +1,10 @@
-var User = require('../api/user/userModel');
-var signToken = require('./auth').signToken;
+var User = require('../api/user/userModel')
+var signToken = require('./auth').signToken
 
-exports.signin = function(req, res, next) {
+exports.signin = function (req, res, next) {
   // req.user will be there from the middleware
   // verify user. Then we can just create a token
   // and send it back for the client to consume
-
-};
+  var token = signToken(req.user._id)
+  res.json({ token: token })
+}
